4b6a1b8d3b58597c3ade5d704596b2a223ca48ec
jaden <806174970@qq.com>
2022-09-25 22:43:48 +0800

    bug

--- src/observer/sql/parser/resolve_stage.cpp
+++ src/observer/sql/parser/resolve_stage.cpp
@@ -106,7 +106,7 @@ void ResolveStage::handle_event(StageEvent *event)
   RC rc = Stmt::create_stmt(db, *query, stmt);
   if (rc != RC::SUCCESS && rc != RC::UNIMPLENMENT) {
     LOG_WARN("failed to create stmt. rc=%d:%s", rc, strrc(rc));
-    session_event->set_response("\n");
+    session_event->set_response("FAILURE\n");
     return;
   }
 
